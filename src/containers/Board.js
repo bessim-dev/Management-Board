5c28252124b506db52c8dc9725af546a8a0c5ad5
becem-dev <you@example.com>
2020-09-27 10:37:34 +0100

    boilerplate for the a manegment board project setup

---
+++ src/containers/Board.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+import Lane from "../components/Lane/Lane";
+import withDataFetching from "../withDataFetching";
+
+const BoardWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 5%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+class Board extends React.Component {
+  state = {
+    tickets: [],
+  };
+  componentDidUpdate(prevProps) {
+    if (prevProps.data !== this.props.data) {
+      this.setState({ tickets: this.props.data });
+    }
+  }
+  onDragStart = (e, id) => {
+    e.dataTransfer.setData("id", id);
+  };
+  onDragOver = (e) => {
+    e.preventDefault();
+  };
+  onDrop = (e, laneId) => {
+    e.preventDefault();
+    const id = e.dataTransfer.getData("id");
+    const tickets = this.state.tickets.filter((ticket) => {
+      if (ticket.id == id) {
+        ticket.lane = laneId;
+      }
+      return ticket;
+    });
+    this.setState({
+      ...this.state,
+      tickets,
+    });
+  };
+  render() {
+    const { lanes, loading, error } = this.props;
+    return (
+      <BoardWrapper>
+        {lanes.map((lane) => (
+          <Lane
+            key={lane.id}
+            laneId={lane.id}
+            title={lane.title}
+            loading={loading}
+            error={error}
+            onDragStart={this.onDragStart}
+            onDragOver={this.onDragOver}
+            onDrop={this.onDrop}
+            tickets={this.state.tickets.filter(
+              (ticket) => ticket.lane === lane.id
+            )}
+          />
+        ))}
+      </BoardWrapper>
+    );
+  }
+}
+
+export default withDataFetching(Board);
